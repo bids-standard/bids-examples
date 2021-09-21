0fbcbe6cebf7b3358e26ff1f687d57a3934d380a
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-09-21 09:48:47 -0400

    RF: Make run_tests.sh logic more clear

--- run_tests.sh
+++ run_tests.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
-rc=0;
-for i in $(ls -d */ | grep -v node_modules);
-do
+rc = 0;
+for i in $(ls -d */ | grep -v node_modules); do
     echo "Validating dataset" $i
 
     if [ -f ${i%%/}/.SKIP_VALIDATION ]; then
-
         echo "Skipping validation for ${i%%/}"
-
-    elif [ -f ${i%%/}/.bids-validator-config.json ]; then
-
-        bids-validator ${i%%/} $VALIDATOR_ARGS --ignoreNiftiHeaders || rc=$?
-
-    else
-
-        if [ $i == "synthetic/" ]; then
-
-            echo "Validating NIfTI headers for dataset" $i
-            bids-validator ${i%%/} $VALIDATOR_ARGS -c $PWD/bidsconfig.json || rc=$?
-
-        else
-
-            bids-validator ${i%%/} $VALIDATOR_ARGS --ignoreNiftiHeaders -c $PWD/bidsconfig.json || rc=$?
-
-        fi
-
+	continue
     fi
+
+    CMD="bids-validator ${i%%/} $VALIDATOR_ARGS"
+
+    # Use default configuration unless overridden
+    if [ ! -f ${i%%/}/.bids-validator-config.json ]; then
+	CMD="$CMD -c $PWD/bidsconfig.json"
+    fi
+
+    # Ignore NIfTI headers except for synthetic dataset
+    if [ $i != "synthetic/" ]; then
+	CMD="$CMD --ignoreNiftiHeaders"
+    else
+        echo "Validating NIfTI headers for dataset" $i
+    fi
+
+    echo $CMD
+    $CMD || rc=$?
 done
 exit $rc;
