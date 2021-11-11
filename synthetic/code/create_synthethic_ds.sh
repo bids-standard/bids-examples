f42c291654838ebf67e4f021c838cdba71ae4f2f
Remi Gau <remi_gau@hotmail.com>
2021-11-11 08:24:28 +0100

    bring back the behavior data

---
+++ synthetic/code/create_synthethic_ds.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# small bash script to create a synthethic BIDS data set
+
+# defines where the BIDS data set will be created
+start_dir=$(pwd)
+raw_dir=${start_dir}/../
+
+subject_list='01 02 03 04 05'
+session_list='01'
+
+create_raw_beh() {
+
+    target_dir=$1
+    subject=$2
+    ses=$3
+
+    suffix='_beh'
+    task_name='stroop'
+
+    this_dir=${target_dir}/sub-${subject}/ses-${ses}/beh
+
+    mkdir -p ${this_dir}
+
+    filename=${this_dir}/sub-${subject}_ses-${ses}_task-${task_name}${suffix}.tsv
+    echo "trial\tresponse\treaction_time\tstim_file" >${filename}
+    echo "congruent\tred\t1.435\timages/word-red_color-red.jpg" >>${filename}
+    echo "incongruent\tred\t1.739\timages/word-red_color-blue.jpg" >>${filename}
+
+}
+
+# RAW DATASET
+for subject in ${subject_list}; do
+    for ses in ${session_list}; do
+        create_raw_beh ${raw_dir} ${subject} ${ses}
+    done
+
+done
